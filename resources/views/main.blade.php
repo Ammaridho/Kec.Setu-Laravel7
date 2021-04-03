4841875504b935bdf1cef93a407d40943ba64f50
Ammaridho <siregarridho28@gmail.com>
2021-04-03 23:58:13 +0700

    Added Kec.Setu_Laravel7

---
+++ resources/views/main.blade.php
@@ -0,0 +1,280 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="app/css/style.css">
+
+    <link rel="stylesheet" href="/css/main.css">
+
+    
+    <title>Kec. Setu- Kota Tangerang Selatan</title>
+  </head>
+  <body>
+
+    <div class="container" id="containerUtama">
+
+        <!-- Header -->
+        
+        <!-- layer 1 -->
+        <div class="row bg-light" id="headerlayer1">
+
+            <div class="col-sm-3" id="divtanggalheader">
+                <input type="text" name="search" value="31 Maret 2021" id="tanggalheader" disabled>
+            </div>
+
+            <div class="col-sm-9" id="divsearchtextinput">
+                <form class="formSearch" id="searchForm" action="" method="get">
+                    <input type="text" name="search" placeholder="Pencarian Berita" id="searchtextinput">
+                    <button type="submit" id="submitSearch">C</button>
+                </form>
+            </div>
+
+        </div>
+
+        <!-- layer 2 -->
+        <div class="bannertop row bg-success">
+            <img src="/img/banner.png" alt="bannertop">
+        </div>
+
+        <!-- layer 3 -->
+        <div class="row" id="headerlayer3">
+            <nav class="navbar navbar-expand-lg navbar-dark" id="navbaratas">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" id="navbaratasCollapse">
+                  <span class="">Menu</span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#" style="line-height: 14px"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+                                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="nav-item active dropdown ml-2" id="ygborder">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Profile
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                      </div>
+                    </li>
+                    <li class="nav-item active dropdown" id="ygborder">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Pelayanan
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item active dropdown" id="ygborder">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Kelurahan
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item active dropdown" id="ygborder">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Produk Hukum
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#" id="ygborder">Berita Kecamatan</a>
+                    </li>
+                    <li class="nav-item active dropdown" id="ygborder">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Gallery
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#" id="ygborder">Pengaduan</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#" id="ygborder">Informasi Publik</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#" id="ygborder">BACK END</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+        </div>
+            
+        <!-- Akhir Header -->
+
+
+        <!-- Mini Header -->
+        <div class="row mt-1">
+            <div class="col-fluid shadow-md p-1" id="miniheader1">
+                <h6 id="hntext">HEADLINE NEWS</h6>
+            </div>
+            <div class="col-fluid shadow-md p-1 bg-light" id="miniheader2">
+                <h6 id="isihntext">Ini adalah berita berita terbaru</h6>
+            </div>
+        </div>
+        <!-- Akhir Mini Header -->
+
+
+        <!-- Content -->
+        <div class="row bg-light p-3 mt-1">
+
+            <div class="col-md-8">
+                
+                <div class="bg-white rounded" id="contentkiri">
+
+                  
+                                        
+                </div>
+        
+            </div>  
+
+            <div class="col-md-4">
+                
+                <div class="tabtab bg-white rounded mb-4" id="contentkanan">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation" id="headtab">
+                          <a class="nav-link active" id="judultab" data-toggle="tab" href="#baru" role="tab" aria-controls="baru" aria-selected="true">Baru</a>
+                        </li>
+                        <li class="nav-item" role="presentation" id="headtab">
+                          <a class="nav-link" id="judultab" data-toggle="tab" href="#tags" role="tab" aria-controls="tags" aria-selected="false">Tags</a>
+                        </li>
+                    </ul>
+                      <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="baru" role="tabpanel" aria-labelledby="baru-tab">
+                            <div class="row">
+
+                                <div class="col-sm-6"  style="padding-left: 10px; padding-top: 10px; margin-top: 5px;">
+                                    <img src="/img/baru1.PNG" alt="error" id="gambartabbaru">
+                                </div>
+                                
+                                <div class="col-sm-6"  style="padding-right: 10px; padding-top: 10px; margin-top: 5px;">
+                                    <p style="font-size:13px; font-weight:400px; line-height:15px; margin-left:-13px;";>Pemkot Tangsel Memperingati hari 17 agustus</p>
+                                    <p style="font-size:11px; margin-top:-10px; margin-left:-13px; color:#B3B3B3;">05 Agustus 2021</p>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="tags" role="tabpanel" aria-labelledby="tags-tab">Tags</div>
+                      </div>
+                </div>
+
+                <h6 id="judulkonten">VIDEO</h6> 
+                <div class="bg-white rounded mb-4 p-2 text-center" id="contentkanan">
+                    <iframe class="embed-responsive-item" style="width: 100%;" src="https://www.youtube.com/embed/QeqbrVFOO74" frameborder="0" allowfullscreen></iframe>
+                    <iframe class="embed-responsive-item" style="width: 100%;" src="https://www.youtube.com/embed/UgX2Kjn8yo4" frameborder="0" allowfullscreen></iframe>
+                </div>     
+
+                <div class="rounded mb-4" id="gambarkanan">
+                </div>
+
+                <h6 id="judulkonten">SOSIAL MEDIA</h6>
+                <div class="bg-white rounded mb-4 p-2 text-center" id="contentkanan">
+                    <img src="/img/icon/youtube.png" alt="iconyoutube" id="iconsm">
+                    <img src="/img/icon/facebook.png" alt="iconfacebook" id="iconsm">
+                    <img src="/img/icon/twitter.png" alt="icontwitter" id="iconsm">
+                    <img src="/img/icon/instagram.png" alt="iconinstagram" id="iconsm">
+                </div>
+
+                <h6 id="judulkonten">TAUTAN</h6>
+                <div class="bg-white rounded mb-4 text-center" id="contentkanan">
+                    <img src="/img/tautan1.PNG" style="width: 250px; border-bottom: 1px solid;" alt="">
+                    <img src="/img/tautan2.PNG" style="width: 250px; border-bottom: 1px solid; padding-bottom: 5px;" alt="">
+                    <img src="/img/tautan3.PNG" style="width: 250px; margin-left:8px; margin-top:4px; margin-bottom: 10px;" alt="">
+                </div>
+            
+            </div>
+                    
+        </div>
+        <!-- Akhir Content -->
+
+        {{-- Footer --}}
+        <div class="row mt-2 pt-3 pl-2 pr-2" id="footer1">
+            <div class="col-sm-4">
+                <h5 id="judulfooter">TAG TERKAIT</h5>
+                <div class="isikontenfooter">
+
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <h5 id="judulfooter">TAUTAN</h5>
+                <div class="isikontenfooter">
+                    <p class="text-light" id="tautanfooter">Pengaduan Masyarakat</p>
+                    <p class="text-light" id="tautanfooter">Website Tangerang Selatan</p>
+                    <p class="text-light" id="tautanfooter">Disdukcapil</p>
+                    <p class="text-light" id="tautanfooter">Dinas Pendidikan Dan Kebudayaan</p>
+                    <p class="text-light" id="tautanfooter">DPMPTSP</p>
+                    <p class="text-light" id="tautanfooter">Polres Tangsel</p>
+                    <p class="text-light" id="tautanfooter">Diskominfo</p>
+                </div>
+                
+            </div>
+            <div class="col-sm-4">
+                <h5 id="judulfooter">ALAMAT KECAMATAN SETU</h5>
+
+                <div class="isikontenfooter">
+                    <p class="text-light" style="font-size: 13px;">Alamat :
+                        Jl. Raya Puspitek, Setu, Kota Tangerang Selatan, Banten 15314</p>
+                    <p class="text-light" style="font-size: 13px;">No Telp : (021) 75791444
+                        E-mail   : kecsetu@tangerangselatankota.go.id</p>
+                    <p class="text-light" style="font-size: 13px;">Waktu Pelayanan:
+                        Senin s/d Jum'at Pukul 08.00 - 15.30
+                        Istirahat Pukul 12.00 - 13.00</p>
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="row" id="footer2">
+            <p style="color: #CCCCCC; font-size:13px; line-height: 50px; text-indent: 10px; font-family: Arial;">&copy Copyright 2020 | Kecamatan Setu - Kota Tangerang Selatan</p>
+        </div>
+        {{-- Akhir Footer --}}
+
+    </div>
+    
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    {{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script> --}}
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
