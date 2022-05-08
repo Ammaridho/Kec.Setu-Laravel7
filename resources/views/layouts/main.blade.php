9750482c91f5b626e9f140085a208b6bc78b5dba
ammaridho <47267524+Ammaridho@users.noreply.github.com>
2022-05-08 14:29:03 +0700

    src fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="shortcut icon" type="image/jpg" href="img/icon/lambang.png"/>
+    <link rel="shortcut icon" type="image/jpg" href="/img/icon/lambang.png"/>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
@@ -17,7 +17,7 @@
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     
-    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="/js/jquery-3.6.0.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
@@ -47,7 +47,6 @@
                 width: 314px;
             }
             #asasasas{
-                /* background-color: #000000; */
                 background-image: url("/img/mobile.png");
             }
             .navbar-collapse{
@@ -201,10 +200,6 @@
             </div>
 
             <!-- layer 2 -->
-
-
-
-
             <div class="bannertop row" >
                 <div class="col"id="asasasas">
                 </div>
